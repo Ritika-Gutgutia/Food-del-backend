159353520a8d6a81e5db41abd02ff3ed76f242ea
Ritika Gutgutia <gutgutiaritika@gmail.com>
2024-08-06 21:33:16 +0530

    final deploy

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -63,8 +63,8 @@ const placeOrder = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       line_items: line_items,
       mode: "payment",
-      success_url: `${frontend_url}/myorders`,
-      cancel_url: `${frontend_url}`,
+      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
     });
 
     console.log(session.url, "SESSION URL");
